664fa2efde5fab574821ee87649d7f2d99064443
Vivek Bhandari <v_bhandari@outlook.com>
2024-11-04 22:28:48 -0500

    use GraphQL server

--- src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
+++ src/main/kotlin/seg3x02/employeeGql/resolvers/EmployeesResolver.kt
@@ -1,8 +1,45 @@
 package seg3x02.employeeGql.resolvers
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.graphql.data.method.annotation.Argument
+import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.stereotype.Controller
+import seg3x02.employeeGql.entity.Employee
+import seg3x02.employeeGql.repository.EmployeesRepository
+import seg3x02.employeeGql.resolvers.types.CreateEmployeeInput
+import java.util.*
 
 @Controller
-class EmployeesResolver {
+class EmployeesResolver(
+    private val employeeRepository: EmployeesRepository
+) {
 
+    @QueryMapping
+    fun employees(): List<Employee> {
+        return employeeRepository.findAll()
+    }
+
+    @MutationMapping
+    fun newEmployee(@Argument("createEmployeeInput") input: CreateEmployeeInput): Employee {
+        if (input.name != null &&
+            input.dateOfBirth != null &&
+            input.city != null &&
+            input.salary != null
+        ) {
+            val employee = Employee(
+                name = input.name,
+                dateOfBirth = input.dateOfBirth,
+                city = input.city,
+                salary = input.salary,
+                gender = input.gender,
+                email = input.email
+            )
+            employee.id = UUID.randomUUID().toString()
+            employeeRepository.save(employee)
+            return employee
+        } else {
+            throw Exception("Invalid input")
+        }
+    }
 }
